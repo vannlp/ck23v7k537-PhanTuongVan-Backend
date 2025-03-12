49c52d789fe7d594562a81e8b33b6bd18262b8db
phanvan0322 <phanvan0322@gmail.com>
2025-03-12 15:34:51 +0700

    Dinh nghia cac route cho tai nguyen Contact

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const cors = require('cors');
 
 const app = express();
 
+const contactsRouter = require("./app/routes/contact.route");
+
 app.use(cors());
 
 app.use(express.json());
@@ -10,5 +12,7 @@ app.use(express.json());
 app.get('/', (req, res) => {
     res.json({ message: 'Welcome to contact book application.' });
 });
+
+app.use("/api/contacts", contactsRouter);
 
 module.exports = app;
